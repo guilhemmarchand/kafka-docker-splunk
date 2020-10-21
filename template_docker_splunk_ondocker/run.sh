7ddc4b0277285a349db8637e9c50bc3908c859bc
Guilhem Marchand <guilhem.marchand@gmail.com>
2020-10-21 12:04:45 +0100

    confluent interceptors, global refresh

--- template_docker_splunk_ondocker/run.sh
+++ template_docker_splunk_ondocker/run.sh
@@ -3,21 +3,21 @@
 docker-compose up --no-start
 
 for container in zookeeper-1 zookeeper-2 zookeeper-3; do
-  docker-compose start $container
+  docker-compose up -d $container
 done
 
 echo "Sleeping 10s..."
 sleep 10
 
 for container in kafka-1 kafka-2 kafka-3; do
-  docker-compose start $container
+  docker-compose up -d $container
 done
 
 echo "Sleeping 10s..."
 sleep 10
 
 for container in kafka-connect-1; do
-  docker-compose start $container
+  docker-compose up -d $container
   echo "Sleeping 5s..."
   sleep 5
 done
@@ -25,6 +25,6 @@ done
 echo "Sleeping 60s..."
 sleep 60
 
-docker-compose start
+docker-compose up -d
 
 exit 0
